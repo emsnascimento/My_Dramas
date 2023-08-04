b3162d9994aa351d48f0d86f927a1b91a778337e
Emilly Nascimento <Emilly.VitoriaCostaNascimento-ext@ab-inbev.com>
2023-08-03 23:06:17 -0300

    Fist settings in the pom.xml

---
+++ src/main/java/com/api/myDramas/MyDramasApplication.java
@@ -0,0 +1,12 @@
+package com.api.myDramas;
+
+import org.springframework.boot.SpringApplication;
+
+public class MyDramasApplication {
+
+    public static void main(final String[] args) {
+
+        SpringApplication.run(MyDramasApplication.class, args);
+
+    }
+}
